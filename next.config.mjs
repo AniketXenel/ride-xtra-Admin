35dc79ed204bdaac2cc9fffa2f6a335ffad90c0f
AniketXenel <aniket.kumar@xenelsoft.co.in>
2026-02-12 10:36:58 +0530

    logo change

--- next.config.mjs
+++ next.config.mjs
@@ -2,27 +2,13 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      // Local development
-      {
-        protocol: 'http',
-        hostname: 'https://ride-xtra-admin-eta.vercel.app/',
-        port: '8082',
-        pathname: '/**',
-      },
-
-      // Production (VM External IP)
       {
         protocol: 'https',
-        hostname: 'https://ride-xtra-admin-eta.vercel.app/',
-        port: '3030', // IMPORTANT if images come from backend
+        hostname: 'ride-xtra-admin-eta.vercel.app',
         pathname: '/**',
       },
     ],
   },
-
-  // // App is served under sub-path
-  // basePath: '/RideXtra',
-  // assetPrefix: '/RideXtra/',
 };
 
 export default nextConfig;
